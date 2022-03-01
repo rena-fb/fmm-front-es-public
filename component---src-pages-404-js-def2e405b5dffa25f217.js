c502c43caaa7a3b91701972921a2cc2ecbde08a1
jaibararia <jaibararia@gmail.com>
2022-03-01 15:18:33 +0530

    spanish site public folder

---
+++ component---src-pages-404-js-def2e405b5dffa25f217.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(t,e,a){a.r(e);var r=a(7294),s=a(9201);e.default=function(){return r.createElement(s.Ar,{className:"error-404"},r.createElement(s.pQ,{title:"404: Not found"}),r.createElement(s.VM,{title:"404: Page Not Found",intro:"You just hit a route that doesn't exist... the sadness"}))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-def2e405b5dffa25f217.js.map
